15a5e5603bd0846dea360deaab9d773ecb605769
Nicolás Gabarrón Blaya <nicolasgabarroncip@gmail.com>
2022-05-13 19:43:04 +0200

    Base del proyecto SPRING.

---
+++ aygp_backend_sql/src/main/java/com/nicogbdev/aygp_backend_sql/AygpBackendSqlApplication.java
@@ -0,0 +1,13 @@
+package com.nicogbdev.aygp_backend_sql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AygpBackendSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AygpBackendSqlApplication.class, args);
+	}
+
+}

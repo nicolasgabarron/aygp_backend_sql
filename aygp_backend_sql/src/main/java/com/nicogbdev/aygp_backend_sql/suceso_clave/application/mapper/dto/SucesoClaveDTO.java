009c66f551e48d0c725ba6452045a6aa9588ec22
Nicolás Gabarrón Blaya <nicolasgabarroncip@gmail.com>
2022-06-18 12:10:53 +0200

    Return complete Date to use it in Frontend.

--- aygp_backend_sql/src/main/java/com/nicogbdev/aygp_backend_sql/suceso_clave/application/mapper/dto/SucesoClaveDTO.java
+++ aygp_backend_sql/src/main/java/com/nicogbdev/aygp_backend_sql/suceso_clave/application/mapper/dto/SucesoClaveDTO.java
@@ -9,9 +9,7 @@ public class SucesoClaveDTO {
     // Propiedades.
     private Long id;
     private Long usuarioId;
-    @JsonFormat(pattern = "dd-MM-yyyy")
     private Date fechaCreacion;
-    @JsonFormat(pattern = "dd-MM-yyyy@HH:mm:ss")
     private Date fechaSuceso;
     private String titulo;
     private String contenido;
